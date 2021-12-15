4532585ba6871aa657c3982ca4158dbecf5ed6d0
Jacqui <jt.cope1@gmail.com>
2021-12-15 18:34:00 +0800

    Transferred code logic to methods file, tested all code. Setup and created txt files for ASCII art and imported to main file

--- src/main.rb
+++ src/main.rb
@@ -1,100 +1,49 @@
 require_relative './methods.rb'
-require 'colorized_string'
-require "tty-prompt"
+# require 'colorized_string'
+require 'tty-prompt'
 require 'lolize'
-require 'colorize'  
-colorizer = Lolize::Colorizer.new 
-
-
+require 'colorize'
+rainbow = Lolize::Colorizer.new
 prompt = TTY::Prompt.new
 
-puts "Welcome to the virtual Chatterbox"
-puts "You have already chosen wisely by opening this app."
-    
+puts "#{banner_ascii}\nWelcome to the virtual Chatterbox\nYou have already chosen wisely by opening this app."
+
 #BASIC MENU SYSTEM
-    choices = { "Start game": 1, 
-                "Christmas Edition": 2, 
-                "What is Chatterbox?": 3, 
-                "Quit": 4}
-    input = prompt.select("Select an option to start", choices)
-    puts input
+choices = {
+  'Start game': 1,
+  'Christmas Edition': 2,
+  'What is Chatterbox?': 3,
+  'Quit': 4
+}
+input = prompt.select('Select an option to start', choices)
+puts input
 
-    
-    case input
-    when 1
-        puts "You have selected to Play".colorize(:magenta)
-        colours = {"Aqua": 1, "Magenta":2, "Yellow": 3, "Emerald": 4}
-        colour_choice = prompt.select("Pick a colour:", colours)
-        puts "You have selected #{colours.key(colour_choice)}"
+case input
+when 1
+  puts 'You have selected to Play'.colorize(:magenta)
+  colours = { 'Blue': 1, 'Magenta': 2, 'Yellow': 3, 'Emerald': 4 }
+  colour_choice = prompt.select('Pick a colour:', colours)
+  puts "You have selected #{colours.key(colour_choice)}"
 
-        # Assigns key names as an uppercase string to the variable letters
-        letters = colours.key(colour_choice).to_s.upcase
+  # Assigns key names as an uppercase string to the variable letters
+  letters = colours.key(colour_choice).to_s.upcase
+  spell_l(letters)
 
-        # loops through the colour selected, printing each letter on a separate line in its colour
-            if letters == "AQUA"
-                letters.split('').each { |l| puts l.colorize(:cyan)}
-            elsif letters == "MAGENTA"
-                letters.split('').each { |l| puts l.colorize(:magenta)}
-            elsif letters == "YELLOW"
-                letters.split('').each { |l| puts l.colorize(:yellow)}
-            else
-                letters.split('').each { |l| puts l.colorize(:green)}
-            end
-           
-        number_choice1 = prompt.select("Pick a number:", random_numbers)
-        puts "#{number_choice1} bonjours for you!"
-        puts "You have selected #{number_choice1}"
-        
-        # Loops through from 1 to number selected. Acts as count actions in game
+  number_choice1 = prompt.select('Pick a number:', random_numbers)
+  puts "You have selected #{number_choice1}"
+  count_nums(number_choice1)
 
-        i = 1
-        loop do
-            puts i
-                i += 1
-            
-            if i == number_choice1 + 1
-                break       
-            end
-        end
-
-        number_choice2 = prompt.select("Pick a number:", random_numbers)
-        puts number_choice2
-        puts "You have selected #{number_choice2}"
-
-        case number_choice2
-
-        when 1
-            colorizer.write "If you eat something and nobody sees you eat it, it has no calories."
-        when 2
-            colorizer.write "Shame on you for looking here for answers to life."
-        when 3
-            colorizer.write "Error 404, fortune not found."
-        when 4
-            colorizer.write "To truly find yourself you should play hide and seek alone."
-        when 5
-            colorizer.write "You will overcome difficult times."
-        when 6
-            colorizer.write "He who never makes mistakes never did anything that’s worthy."
-        when 7
-            colorizer.write "You will marry a professional athlete - if competitive eating can be considered a sport."
-        else
-            colorizer.write "Perhaps you’ve been focusing too much on spending."
-        end
-
-        
-    when 2
-        puts "You have opted for the Christmas Edition".colorize(:green)
-
-    when 3
-        puts "You seek further understanding of what is Chatterbox".colorize(:orange)
-
-    when 4
-        puts "Goodbye".colorize(:pink)
-        exit
-
-    else
-        puts "Please pick 1, 2, 3 or 4".colorize(:red)
-    end
-
-
-    
+  number_choice2 = prompt.select('Pick a number:', random_numbers)
+  puts fortune_ascii_art
+  puts "You have selected #{number_choice2}"
+  fortune(number_choice2, rainbow)
+when 2
+  puts 'You have opted for the Christmas Edition'.colorize(:green)
+when 3
+  puts 'You seek further understanding of what is Chatterbox'.colorize(:orange)
+when 4
+  puts 'Goodbye'.colorize(:pink)
+  exit
+else
+  puts 'Please pick 1, 2, 3 or 4'.colorize(:red)
+end
--- src/methods.rb
+++ src/methods.rb
@@ -1,6 +1,62 @@
+require 'colorize'
+
+# prints banner in welcome page
+def banner_ascii
+  puts File.read('./banner.txt')
+end
+
 # returns an array of 4 random numbers from 1-8
 def random_numbers
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-    random_nums = numbers.shuffle[0,4]
-    return random_nums
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+  random_nums = numbers.shuffle[0, 4]
+  return random_nums
+end
+
+# loops through the colour selected, printing each letter on a separate line in its colour
+def spell_l(letters)
+  if letters == 'BLUE'
+    letters.split('').each { |l| puts l.colorize(:cyan) }
+  elsif letters == 'MAGENTA'
+    letters.split('').each { |l| puts l.colorize(:magenta) }
+  elsif letters == 'YELLOW'
+    letters.split('').each { |l| puts l.colorize(:yellow) }
+  else
+    letters.split('').each { |l| puts l.colorize(:green) }
+  end
+end
+
+# Loops through from 1 to number selected. Acts as count actions in game
+def count_nums(number_choice1)
+  i = 1
+  loop do
+    puts i
+    i += 1
+
+    break if i == number_choice1 + 1
+  end
+end
+
+def fortune(number_choice2, rainbow)
+  case number_choice2
+  when 1
+    rainbow.write 'If you eat something and nobody sees you eat it, it has no calories.'
+  when 2
+    rainbow.write 'Shame on you for looking here for answers to life.'
+  when 3
+    rainbow.write 'Error 404, fortune not found.'
+  when 4
+    rainbow.write 'To truly find yourself you should play hide and seek alone.'
+  when 5
+    rainbow.write 'You will overcome difficult times.'
+  when 6
+    rainbow.write 'He who never makes mistakes never did anything that’s worthy.'
+  when 7
+    rainbow.write 'You will marry a professional athlete - if competitive eating can be considered a sport.'
+  else
+    rainbow.write 'Perhaps you’ve been focusing too much on spending.'
+  end
+end
+
+def fortune_ascii_art
+  puts File.read('./chatterbox-ascii.txt')
 end
